f0b346d587819e775720d1051d3b54487aefe7a8
dsbrgg <dsbrgg@gmail.com>
2020-03-06 15:00:45 +0000

    Update product tasks with DomainTask

--- saleor/product/tasks.py
+++ saleor/product/tasks.py
@@ -11,6 +11,8 @@ from .utils.variant_prices import (
     update_products_minimal_variant_prices_of_discount,
 )
 
+from ..domain_task import DomainTask
+from ..domain_utils import setup_celery_connection
 
 def _update_variants_names(instance: ProductType, saved_attributes: Iterable):
     """Product variant names are created from names of assigned attributes.
@@ -34,37 +36,58 @@ def _update_variants_names(instance: ProductType, saved_attributes: Iterable):
         variant.save(update_fields=["name"])
 
 
-@app.task
-def update_variants_names(product_type_pk: int, saved_attributes_ids: List[int]):
+@app.task(base=DomainTask)
+def update_variants_names(product_type_pk: int, saved_attributes_ids: List[int], **kwargs):
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     instance = ProductType.objects.get(pk=product_type_pk)
     saved_attributes = Attribute.objects.filter(pk__in=saved_attributes_ids)
     _update_variants_names(instance, saved_attributes)
 
 
-@app.task
-def update_product_minimal_variant_price_task(product_pk: int):
+@app.task(base=DomainTask)
+def update_product_minimal_variant_price_task(product_pk: int, **kwargs):
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     product = Product.objects.get(pk=product_pk)
     update_product_minimal_variant_price(product)
 
 
-@app.task
+@app.task(base=DomainTask)
 def update_products_minimal_variant_prices_of_catalogues_task(
     product_ids: Optional[List[int]] = None,
     category_ids: Optional[List[int]] = None,
     collection_ids: Optional[List[int]] = None,
+    **kwargs
 ):
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     update_products_minimal_variant_prices_of_catalogues(
         product_ids, category_ids, collection_ids
     )
 
 
-@app.task
-def update_products_minimal_variant_prices_of_discount_task(discount_pk: int):
+@app.task(base=DomainTask)
+def update_products_minimal_variant_prices_of_discount_task(discount_pk: int, **kwargs):
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     discount = Sale.objects.get(pk=discount_pk)
     update_products_minimal_variant_prices_of_discount(discount)
 
 
-@app.task
-def update_products_minimal_variant_prices_task(product_ids: List[int]):
+@app.task(base=DomainTask)
+def update_products_minimal_variant_prices_task(product_ids: List[int], **kwargs):
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     products = Product.objects.filter(pk__in=product_ids)
     update_products_minimal_variant_prices(products)
