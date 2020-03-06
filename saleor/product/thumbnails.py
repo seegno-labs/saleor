5c021b74daec8cae823e61587022e7d2cb2af4fe
dsbrgg <dsbrgg@gmail.com>
2020-03-06 15:01:10 +0000

    Update product thumbnail tasks with DomainTask

--- saleor/product/thumbnails.py
+++ saleor/product/thumbnails.py
@@ -1,17 +1,26 @@
 from ..celeryconf import app
 from ..core.utils import create_thumbnails
+from ..domain_task import DomainTask
+from ..domain_utils import setup_celery_connection
 from .models import Category, Collection, ProductImage
 
-
-@app.task
-def create_product_thumbnails(image_id: str):
+@app.task(base=DomainTask)
+def create_product_thumbnails(image_id: str, **kwargs):
     """Take a ProductImage model and create thumbnails for it."""
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     create_thumbnails(pk=image_id, model=ProductImage, size_set="products")
 
 
-@app.task
-def create_category_background_image_thumbnails(category_id: str):
+@app.task(base=DomainTask)
+def create_category_background_image_thumbnails(category_id: str, **kwargs):
     """Take a Product model and create the background image thumbnails for it."""
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     create_thumbnails(
         pk=category_id,
         model=Category,
@@ -20,9 +29,13 @@ def create_category_background_image_thumbnails(category_id: str):
     )
 
 
-@app.task
-def create_collection_background_image_thumbnails(collection_id: str):
+@app.task(base=DomainTask)
+def create_collection_background_image_thumbnails(collection_id: str, **kwargs):
     """Take a Collection model and create the background image thumbnails for it."""
+    domain = kwargs.get('domain')
+
+    setup_celery_connection(domain)
+
     create_thumbnails(
         pk=collection_id,
         model=Collection,
